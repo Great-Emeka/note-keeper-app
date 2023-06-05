c3722f4b6523af7f2cfae657a59ad75d0618f986
great-emeka <greatestgreatemeka@gmail.com>
2023-06-05 13:04:36 +0300

    Updates

---
+++ precache-manifest.a5e0f33c3b93f6a6168c668c571590b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4149911ad2755f1846e",
+    "url": "/static/css/main.b376893b.chunk.css"
+  },
+  {
+    "revision": "b4149911ad2755f1846e",
+    "url": "/static/js/main.b4149911.chunk.js"
+  },
+  {
+    "revision": "c95dbf028f30eda09fb8",
+    "url": "/static/js/1.c95dbf02.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f876cb86b5c6e7923d75903936e8f5d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/note-keeper-app/precache-manifest.2d5b50d6be7aa480b23384f5896faddc.js"
+  "/precache-manifest.a5e0f33c3b93f6a6168c668c571590b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/note-keeper-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
